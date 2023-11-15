a6318932bfe9f9c78e1fbc4a46ee1a96b801d8c9
5eoyeonn <150306426+5eoyeonn@users.noreply.github.com>
2023-11-15 09:28:14 +0900

    Arduino.jsx

--- src/Component/Career/Arduino.jsx
+++ src/Component/Career/Arduino.jsx
@@ -2,25 +2,31 @@
 const Arduino = () => {
   return (
     <div>
-      <h1>아르바이트 및 인턴</h1>
+      <h1>아두이노</h1>
       <fieldset>
-        <legend>아르바이트</legend>
+        <legend>활동 시작</legend>
         <ul>
-          <li>아르바이트에 대한 내용을 작성</li>
-          <li>아르바이트에 대한 내용을 작성</li>
+          <li>아두이노 - 아두이노 키트 활용해서 전구, 신호등, 부저 등 부속품을 용도에 맞춰 코딩하기</li>
+          <li>아두이노 키트 구성 : </li>
+          <li>아두이노 보드 - 아두이노의 뇌 역할 수행</li>
+          <li>USB 케이블 - 아두이노 보드를 컴퓨터와 연결</li>
+          <li>점프 와이어 (점퍼선) - 회로를 연결하고 부품을 연결하는 데 사용되는 선</li>
+          <li>저항 (Resistors) - LED 밝기를 제어하거나 전압 분배</li>
+          <li>LED (Light Emitting Diode) - 시각적 표시</li>
+          <li>부저 (Buzzer) - 소리를 생성하거나 경고음을 발생</li>
+          <li>기타 등등의 구성품을 연결하고 아두이노 IDE를 통해 코딩함으로써 결과물을 창출. 아래는 활동 과정 사진 </li>
         </ul>
       </fieldset>
 
       <fieldset>
-        <legend>인턴</legend>
+        <legend>활동 과정</legend>
         <ul>
-          <li>인턴에 대한 내용을 작성</li>
-          <li>인턴에 대한 내용을 작성</li>
+          <li></li>
+          <li></li>
         </ul>
       </fieldset>
     </div>
   );
 };
 
-export default Career1;
-
+export default Arduino;
