4889fa5db457f749f82ef7abc788338ab5bf88f4
5eoyeonn <150306426+5eoyeonn@users.noreply.github.com>
2023-11-15 09:28:49 +0900

    Entry.jsx

--- src/Component/Career/Entry.jsx
+++ src/Component/Career/Entry.jsx
@@ -1,25 +1,16 @@
-
 const Entry = () => {
   return (
     <div>
-      <h1>사회 봉사 및 연수 </h1>
+      <h1>엔트리 </h1>
       <fieldset>
-        <legend>사회 봉사</legend>
+        <legend>활동 내용</legend>
         <ul>
-          <li>사회 봉사에 대한 내용을 작성</li>
-          <li>사회 봉사에 대한 내용을 작성</li>
-        </ul>
-      </fieldset>
-
-      <fieldset>
-        <legend>해외 연수</legend>
-        <ul>
-          <li>해외 연수에 대한 내용을 작성</li>
-          <li>해외 연수에 대한 내용을 작성</li>
+          <li>엔트리란</li>
+          <li>기존의 텍스트 코딩과 달리 블록을 끼우는 방식으로 코딩할 수 있는 그래픽 기반 프로그램으로 학교에서의 코딩 교육 플랫폼이다. 주로 초등학교의 실과 시간, 중학교의 정보 과목에서 상당한 분량을 차지한다. 또한, 대학교에서도 주로 신입생들 컴퓨팅 사고 교양필수 강좌에서도 다수 활용된다.</li>
         </ul>
       </fieldset>
     </div>
   );
 };
 
-export default Career2;
+export default Entry;
